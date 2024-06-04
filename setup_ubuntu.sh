16f603adc1d9dd7f718c06218d9c02d12a91ca48
jiepengtan <jiepengtan@gmail.com>
2024-06-04 11:13:19 +0800

    update ubuntu setup scripts

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -1,16 +1,8 @@
-# Copyright (c) Meta Platforms, Inc. and affiliates.
-# This source code is licensed under the MIT license found in the
-# LICENSE file in the root directory of this source tree.
-
-# needed for torchserve
-# if no java..
-if ! command -v java &> /dev/null
-then
-	sudo apt install openjdk-19-jre-headless
-fi
-
-#conda install pytorch==2.0.0 torchvision==0.15.0 torchaudio==2.0.0 pytorch-cuda=11.8 -c pytorch -c nvidia
 #!/bin/bash
+
+#conda create -n drawing python=3.8.13 -y
+#conda install pytorch==2.0.0 torchvision==0.15.0 torchaudio==2.0.0 pytorch-cuda=11.8 -c pytorch -c nvidia
+
 
 # install os dependencies
 sudo apt-get update 
@@ -18,8 +10,6 @@ sudo apt install ca-certificates
 sudo apt install default-jre
 
 # install python dependencies
-#conda install python=3.8.13 -y
-
 pip install openmim
 pip install torch==2.0.0
 mim install mmcv-full==1.7.0
@@ -39,9 +29,12 @@ cd ../
 pip install mmpose==0.29.0
 pip install torchvision==0.15.1  # solve torch version problem
 
+ckpt_dir="/home/torchserve/model-store"
+mkdir -p $ckpt_dir
 if [ ! -d "model-store" ]; then
-	wget https://github.com/facebookresearch/AnimatedDrawings/releases/download/v0.0.1/drawn_humanoid_detector.mar -P ./model-store/
-	wget https://github.com/facebookresearch/AnimatedDrawings/releases/download/v0.0.1/drawn_humanoid_pose_estimator.mar -P ./model-store/
+	echo "download checkpoints to " $ckpt_dir
+	wget https://github.com/facebookresearch/AnimatedDrawings/releases/download/v0.0.1/drawn_humanoid_detector.mar -P $ckpt_dir/
+	wget https://github.com/facebookresearch/AnimatedDrawings/releases/download/v0.0.1/drawn_humanoid_pose_estimator.mar -P $ckpt_dir/
 fi
 
 echo "*** Now run torchserve:"
