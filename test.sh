c0fc220d9b83b454d38a98bab8fe07f4ac914759
jiepengtan <jiepengtan@gmail.com>
2024-06-04 11:06:02 +0800

    init

---
+++ test.sh
@@ -0,0 +1,4 @@
+#python test.py
+
+wget https://github.com/facebookresearch/AnimatedDrawings/releases/download/v0.0.1/drawn_humanoid_detector.mar -P ./model-store/
+wget https://github.com/facebookresearch/AnimatedDrawings/releases/download/v0.0.1/drawn_humanoid_pose_estimator.mar -P ./model-store/
