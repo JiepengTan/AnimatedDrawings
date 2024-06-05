00f0892e36ae90f7e690764ad590fa515a0e7c19
jiepengtan <jiepengtan@gmail.com>
2024-06-05 14:01:46 +0800

    remove useless script

--- test.sh
+++
@@ -1,4 +0,0 @@
-#python test.py
-
-wget https://github.com/facebookresearch/AnimatedDrawings/releases/download/v0.0.1/drawn_humanoid_detector.mar -P ./model-store/
-wget https://github.com/facebookresearch/AnimatedDrawings/releases/download/v0.0.1/drawn_humanoid_pose_estimator.mar -P ./model-store/
